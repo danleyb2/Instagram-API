ed17305f526af7722fa00c8e690ba66fd9a40e89
qsniyg <qsniyg@mail.com>
2017-03-05 10:49:37 -0800

    Match SignatureUtils.generateUUID with php repo

--- InstagramAPI/src/SignatureUtils.py
+++ InstagramAPI/src/SignatureUtils.py
@@ -21,11 +21,14 @@ class SignatureUtils:
     @staticmethod
     def generateUUID(type):
         uuid = '%04x%04x-%04x-%04x-%04x-%04x%04x%04x' % (
-            mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
             mt_rand(0, 0xffff),
             mt_rand(0, 0x0fff) | 0x4000,
             mt_rand(0, 0x3fff) | 0x8000,
-            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff)
         )
 
         return uuid if type else uuid.replace('-', '')
