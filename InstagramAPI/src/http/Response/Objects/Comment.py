6e4267e36440f89e68c8e6706705e35124a9c20f
qsniyg <qsniyg@mail.com>
2017-03-05 13:44:25 -0800

    Add str specialization for Response/Objects/Comment

--- InstagramAPI/src/http/Response/Objects/Comment.py
+++ InstagramAPI/src/http/Response/Objects/Comment.py
@@ -5,12 +5,15 @@ from .User import User
 class Comment(Response):
     def __init__(self):
         self._types = {}
+
         self.status = None
+        self._types["username_id"] = str
         self.username_id = None
         self.created_at_utc = None
         self.created_at = None
         self.bit_flags = None
         self._types["user"] = User
         self.user = None
+        self._types["pk"] = str
         self.pk = None
         self.text = None
