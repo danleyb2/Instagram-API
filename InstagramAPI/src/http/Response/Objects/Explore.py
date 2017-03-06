4f765e1caa73f78b65bc3ab79d089b9fc3564846
qsniyg <qsniyg@mail.com>
2017-03-05 16:07:58 -0800

    Add str type specification for Response/Objects/Explore

--- InstagramAPI/src/http/Response/Objects/Explore.py
+++ InstagramAPI/src/http/Response/Objects/Explore.py
@@ -3,6 +3,9 @@ from InstagramAPI.src.http.Response.Response import Response
 
 class Explore(Response):
     def __init__(self):
+        self._types = {}
+
         self.explanation = None
+        self._types["actor_id"] = str
         self.actor_id = None
         self.source_token = None
